170e1d52998012e339683d5e354d5c8df53b75bb
Pranav Raj <rpranav955@gmail.com>
2026-03-10 20:52:37 +0530

    Refactor bankers algorithm for dynamic input

--- bankers.c
+++ bankers.c
@@ -2,68 +2,88 @@
 
 int main()
 {
-    int n = 5, m = 3;
-    int alloc[5][3] = {
-        {0, 1, 0},
-        {2, 0, 0},
-        {3, 0, 2},
-        {2, 1, 1},
-        {0, 0, 2}};
+    int n, m;
+    printf("Enter the number of proc: ");
+    scanf("%d", &n);
+    printf("Enter the number of res: ");
+    scanf("%d", &m);
 
-    int max[5][3] = {
-        {7, 5, 3},
-        {3, 2, 2},
-        {9, 0, 2},
-        {2, 2, 2},
-        {4, 3, 3}};
-
-    int avail[3] = {3, 3, 2};
-    int need[5][3];
-    int finish[5] = {0};
-    int safeSeq[5];
-    int work[3];
-
-    for (int i = 0; i < m; i++)
-        work[i] = avail[i];
+    int alloc[n][m], max[n][m], avail[m], need[n][m], finish[n];
 
     for (int i = 0; i < n; i++)
+    {
+        printf("Enter the alloc res for P%d: \n", i);
         for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &alloc[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the max res for P%d: \n", i);
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &max[i][j]);
+        }
+    }
+
+    printf("Enter the available %d resources: ", m);
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &avail[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        finish[i] = 0;
+        for (int j = 0; j < m; j++)
+        {
             need[i][j] = max[i][j] - alloc[i][j];
+        }
+    }
 
     int count = 0;
+    int safeSeq[n];
+
     while (count < n)
     {
         int found = 0;
         for (int i = 0; i < n; i++)
         {
-            if (!finish[i])
+            if (finish[i] != 1)
             {
                 int j;
                 for (j = 0; j < m; j++)
-                    if (need[i][j] > work[j])
+                {
+                    if (need[i][j] > avail[j])
+                    {
                         break;
-
+                    }
+                }
                 if (j == m)
                 {
                     for (int k = 0; k < m; k++)
-                        work[k] += alloc[i][k];
-
-                    safeSeq[count++] = i;
+                    {
+                        avail[k] += alloc[i][k];
+                    }
                     finish[i] = 1;
+                    safeSeq[count++] = i;
                     found = 1;
                 }
             }
         }
-        if (!found)
+        if (found != 1)
         {
-            printf("System is in UNSAFE state\n");
+            printf("IT IS UNDER UNSAFE SEQUANCE\n");
             return 0;
         }
     }
 
-    printf("System is in SAFE state\nSafe sequence: ");
+    printf("THERE IS A SAFE SEQUANCE: ");
     for (int i = 0; i < n; i++)
+    {
         printf("P%d ", safeSeq[i]);
-
+    }
     return 0;
 }
